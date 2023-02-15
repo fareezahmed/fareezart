55c1a2aab29c5e2d5a74d0e55ae87ecaf02b71b2
Fareez <fareez1234@gmail.com>
2023-02-15 14:26:51 +1100

    Fix Build Issue

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+}
+
+module.exports = nextConfig
