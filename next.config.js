afe20cb5b2011fe1b4e145951afcbf996d06df87
Fareez <fareez.ahmed@attendanywhere.com>
2024-02-04 19:01:58 +1100

    Basic Website setup with tailwind and i18n

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config.js');
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
+  i18n,
   images: {
     unoptimized: true,
   },
